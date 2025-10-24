9161832a9f05fc6e48819b481881ed1ac0da8c96
Sergei Popov <p.sergey_n@vk.com>
2025-10-25 00:43:35 +0500

    remove default error handling middleware from index.js

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
-});
-
 app.listen(PORT, () => {
 	console.log(`Server running on port: http://localhost:${PORT}`)
 })
